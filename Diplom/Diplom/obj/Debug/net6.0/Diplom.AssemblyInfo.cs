cd8874fc3ce478a0126791d29a04cbc45cfcaf13
ShizoFX <alessandro.davare@htl-saalfelden.at>
2024-01-18 11:03:32 +0100

    Webapp vorübergehend Fertiggestellt
    Funktionen müssen später geupdated werden

--- Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
+++ Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e79ef65f360b3e0be3b7ca104fdab20a51ca77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
