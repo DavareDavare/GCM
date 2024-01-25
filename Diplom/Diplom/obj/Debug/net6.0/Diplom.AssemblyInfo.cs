6937a7b6d7c7ce25bc6e83c9b7e54897e03d0eb7
ShizoFX <47882058+DavareDavare@users.noreply.github.com>
2024-01-25 14:38:37 +0100

    Provisorisches Light Theme Webapp

--- Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
+++ Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e79ef65f360b3e0be3b7ca104fdab20a51ca77a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7209c71ae03eacbf73ce89c89a09095375d78978")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
