f4752fc5bec17301c860966206adb15f3e67df9d
ShizoFX <alessandro.davare@htl-saalfelden.at>
2024-02-01 12:20:50 +0100

    Placeholder "..."

--- Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
+++ Diplom/Diplom/obj/Debug/net6.0/Diplom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7209c71ae03eacbf73ce89c89a09095375d78978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b3bd36b0f85c3063ae8cb1c46f625cbe3c276b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
