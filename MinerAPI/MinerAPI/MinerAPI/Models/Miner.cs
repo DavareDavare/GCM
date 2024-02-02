0887344b9d8a2375c02ad40a31babc51e9c070c2
ShizoFX <47882058+DavareDavare@users.noreply.github.com>
2024-02-02 10:13:15 +0100

    Simple API noch ohne DB, Swagger in Dokumentation

---
+++ MinerAPI/MinerAPI/MinerAPI/Models/Miner.cs
@@ -0,0 +1,37 @@
+﻿namespace MinerAPI.Models
+{
+    public class Miner
+    {
+        public int Id { get; set; }
+        public string Ip { get; set; }
+        public string Group { get; set; }
+        public string AdminName { get; set; }
+        public string AdminPassword { get; set; }
+        public float HashRate { get; set; }
+
+        // Constructor with parameters
+        public Miner(string ip, string group, string adminName, string adminPassword)
+        {
+            Id = 0; // The server will assign an Id
+            Ip = ip;
+            Group = group;
+            AdminName = adminName;
+            AdminPassword = adminPassword;
+            HashRate = 0;
+        }
+
+        // Default constructor
+        public Miner()
+        {
+            // Default values if needed
+            Id = 0;
+            Ip = "";
+            Group = "";
+            AdminName = "";
+            AdminPassword = "";
+            HashRate = 0;
+        }
+
+    }
+
+}
