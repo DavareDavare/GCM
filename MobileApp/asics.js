df32eec473d65d8bfe9f51bac27ac4c8efb48e31
Sanna0908 <sarahanna0908@gmx.at>
2024-02-08 07:37:48 +0100

    App-Api Connection

--- MobileApp/asics.js
+++
@@ -1,5 +0,0 @@
-/*
-  Author: Sarah Hagenhofer
-  Lastly edited: 14.01.2023
-  Comment: JS file for allasics.html
-*/
--- PVAnlageAPI/PVAnlageApi/obj/Debug/net8.0/PVAnlageApi.AssemblyInfo.cs
+++ PVAnlageAPI/PVAnlageApi/obj/Debug/net8.0/PVAnlageApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcea6bd8f0606778eb649d7deb2fb12c9db0b77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
