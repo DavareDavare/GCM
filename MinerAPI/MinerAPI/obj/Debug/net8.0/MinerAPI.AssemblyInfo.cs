aa4a7cd9abbd4cd884b495d7507d047bc1503403
Sanna0908 <sarahanna0908@gmx.at>
2024-02-29 10:23:45 +0100

    Dokumentation Mobile-App

--- MinerAPI/MinerAPI/obj/Debug/net8.0/MinerAPI.AssemblyInfo.cs
+++ MinerAPI/MinerAPI/obj/Debug/net8.0/MinerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828ac5c588fff1936eb94d9170e1c10d06ef0631")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
