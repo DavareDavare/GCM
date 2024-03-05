4f693a8638c536610f3ab9fe58b9659a8d04b5c5
Sanna0908 <sarahanna0908@gmx.at>
2024-03-05 16:20:31 +0100

    Dokumentation

--- MobileApp/switch.js
+++ MobileApp/switch.js
@@ -151,8 +151,8 @@ function switchNiederschlag(){
 
   if(statusniederschlag==0){
 
+
     statusniederschlag=1;
-    var usedicon;
 
     var geturl = 'https://api.open-meteo.com/v1/forecast?latitude=47.3441&longitude=13.3915&current=precipitation&forecast_days=1';
 
@@ -185,9 +185,6 @@ function switchNiederschlag(){
       });
 
 
-    //creates a new table with the weather data / existing error: only a placeholder up until now, yet to be filled with real weather data
-    let divclass = document.getElementById('AktuellerNiederschlag');
-    divclass.innerHTML = '<table class="col-12 col-md-8" id="showweather" style="text-align: center; width: 200px; font-size: larger;"><thead><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16"><path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/></svg></thead></table>';
 
     //closes the two other data-sections if opened
     if(statusHash==1){
