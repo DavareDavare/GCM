90c9c8d02ab19d6f9109b571df3d5bf9f2362d40
Sanna0908 <sarahanna0908@gmx.at>
2024-03-08 14:57:41 +0100

    Rate-Graph

--- PVAnlageAPI/PVAnlageApi/obj/Debug/net8.0/PVAnlageApi.AssemblyInfo.cs
+++ PVAnlageAPI/PVAnlageApi/obj/Debug/net8.0/PVAnlageApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0dcd00503e6ce3cfb6f3e0041231c4d286ef8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc749608de02a516525edfb04cfbb2dcc7a23b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PVAnlageApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
