1ddcd022f358b65e2a3e027a1a61325e0cfca5df
Sanna0908 <sarahanna0908@gmx.at>
2024-03-12 10:13:26 +0100

    Diplomi, Graph Hash

--- MobileApp/switch.js
+++ MobileApp/switch.js
@@ -111,24 +111,13 @@ function switchRate(){
 
 
 function switchHash(){
-    
+
   if(statusHash==0){
       
     statusHash = 1;
 
     var curr;
-    var oneAgo;
-    var twoAgo ;
-    var threeAgo;
-    var fourAgo;
-    var fiveAgo;
-    var currDate = new Date();
-    var currTime = currDate.getHours()+":"+currDate.getMinutes()+":"+currDate.getSeconds();
-    var oneAgoTime = (currDate.getHours()-4)+":"+currDate.getMinutes()+":"+currDate.getSeconds();
-    var twoAgoTime = (currDate.getHours()-8)+":"+currDate.getMinutes()+":"+currDate.getSeconds();
-    var threeAgoTime = (currDate.getHours()-12)+":"+currDate.getMinutes()+":"+currDate.getSeconds();
-    var fourAgoTime = (currDate.getHours()-16)+":"+currDate.getMinutes()+":"+currDate.getSeconds();
-    var fiveAgoTime = (currDate.getHours()-20)+":"+currDate.getMinutes()+":"+currDate.getSeconds();
+    var point;
 
     var geturl = 'https://localhost:7294/api/Miner/GetMiner';
 
@@ -140,64 +129,37 @@ function switchHash(){
         return response.json();
       })
       .then(data=>{
+        
         var datalength = data.length-1;
-          curr = data[datalength].hashRate;
-          oneAgo = data[datalength-1].hashRate;
-          twoAgo = data[datalength-2].hashRate;
-          threeAgo = data[datalength-3].hashRate;
-          fourAgo = data[datalength-4].hashRate;
-          fiveAgo = data[datalength-5].hashRate;
+        var all = [];
 
+        for(var i=0; i<=datalength; i++){
+            curr = data[i].hashRate;
+            point = i+1;
+
+            all.push({which: point, value: curr});
+        }
+          
           let divclass = document.getElementById('hash');
           divclass.querySelector("h2").innerHTML="";
           let chartclass = document.getElementById('chartDivHash');
           chartclass.style="position: relative; height:30vh; width:60vw;";
-      
+
           //creates a line-chart with the given data / existing error: is yet to be filled with the real data, up until now works based on proto-data
           var chart = JSC.chart('chartDivHash', {
               debug: true,
               type: 'line',
               legend_position: 'inside bottom right',
-              xAxis: { type: 'time' },
               series: [
                 {
-                  //so soll es sein, Rest ist derweil nur Beispielwert
-                  name: 'Miner1',
+                  name: 'Miners',
                   points: [
-                    [fiveAgoTime, fiveAgo],
-                    [fourAgoTime, fourAgo],
-                    [threeAgoTime, threeAgo],
-                    [twoAgoTime, twoAgo],
-                    [oneAgoTime, oneAgo],
-                    [currTime, curr]
-                  ]
-                },
-                {
-                  name: 'Miner2',
-                  points: [
-                    [fiveAgoTime, fiveAgo],
-                    [fourAgoTime, fourAgo],
-                    [threeAgoTime, threeAgo],
-                    [twoAgoTime, curr],
-                    [oneAgoTime, curr],
-                    [currTime, curr]
-                  ]
-                },
-                {
-                  name: 'Miner3',
-                  points: [
-                    [fiveAgoTime, curr],
-                    [fourAgoTime, curr],
-                    [threeAgoTime, threeAgo],
-                    [twoAgoTime, twoAgo],
-                    [oneAgoTime, oneAgo],
-                    [currTime, curr]
+                    [all[0].which, all[0].value],
+                    [all[1].which, all[1].value]
                   ]
                 }
               ]
-            });
-
-
+          }); 
       })
       .catch(error=>{
         console.log('Error', error);
@@ -251,13 +213,13 @@ function switchNiederschlag(){
         var getniederschlag = data.current.precipitation;
         let divclass = document.getElementById('AktuellerNiederschlag');
 
-        if(Number(getniederschlag).toFixed(2)<=1){
+        if(Number(getniederschlag).toFixed(2)<=0.05){
           divclass.innerHTML = '<table class="col-12 col-md-8" id="showweather" style="text-align: center; width: 200px; font-size: larger;"><thead><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16"><path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/></svg></thead></table>';
         }
-        else if(Number(getniederschlag).toFixed(2)>1 && Number(getniederschlag).toFixed(2)<=10){
+        else if(Number(getniederschlag).toFixed(2)>0.05 && Number(getniederschlag).toFixed(2)<=2){
           divclass.innerHTML = '<table class="col-12 col-md-8" id="showweather" style="text-align: center; width: 200px; font-size: larger;"><thead><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cloud-fill" viewBox="0 0 16 16"><path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/></svg></thead></table>';
         }
-        else if(Number(getniederschlag).toFixed(2)>10){
+        else if(Number(getniederschlag).toFixed(2)>2){
           divclass.innerHTML = '<table class="col-12 col-md-8" id="showweather" style="text-align: center; width: 200px; font-size: larger;"><thead><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cloud-hail-fill" viewBox="0 0 16 16"><path d="M3.75 15.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0m.408-3.724a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316M7.75 15.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0m.408-3.724a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316m3.592 3.724a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0m.408-3.724a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316m1.247-6.999a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10.5H13a3 3 0 0 0 .405-5.973"/></svg></thead></table>';
         }
         else{
